e45d71f944375c5600b4b12f4e717ed1ec9778e9
Richlyn <39412334+Richlyn@users.noreply.github.com>
2019-05-24 11:29:31 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secret: "supersecret",
-  google: {
-    clientID:
-      "1043782365319-bthl7e0v6qgvcj4hbhvpcgbqtticapn7.apps.googleusercontent.com",
-    clientSecret: "gBw5O-obr9uNSlHUy1FMy3S3"
-  }
-};
